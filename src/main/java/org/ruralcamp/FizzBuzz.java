9016253b0569fdcec0c28ab8cc9a960e5d9a9465
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-02-26 11:27:10 +0100

    tests pasados

--- src/main/java/org/ruralcamp/FizzBuzz.java
+++ src/main/java/org/ruralcamp/FizzBuzz.java
@@ -1,17 +1,15 @@
 package org.ruralcamp;
 
 public class FizzBuzz {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+    public String fizzBuzz(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FizzBuzz";
+        } else if (num % 3 == 0) {
+            return "Fizz";
+        } else if (num % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(num);
         }
     }
 }
